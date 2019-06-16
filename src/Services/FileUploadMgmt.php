154a13274a4966ef7fd2d74779622a27d26f051f
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2019-06-16 17:24:56 +0700

    Fix coding style

--- src/Services/FileUploadMgmt.php
+++ src/Services/FileUploadMgmt.php
@@ -13,18 +13,18 @@ use Bkstar123\LaravelUploader\Services\FileUpload;
 
 class FileUploadMgmt extends FileUpload
 {
-	/**
-	 * Physically remove an uploaded file
-	 * Should be called after removing the associated database record
-	 * @param $disk      string
-	 * @param $filepath  $string
-	 * @return void
-	 */
-	public function delete($disk, $filepath)
-	{
-		Storage::disk($disk)->delete($filepath);
-		$this->deleteFileContainer($disk, dirname($filepath));
-	}
+    /**
+     * Physically remove an uploaded file
+     * Should be called after removing the associated database record
+     * @param $disk      string
+     * @param $filepath  $string
+     * @return void
+     */
+    public function delete($disk, $filepath)
+    {
+        Storage::disk($disk)->delete($filepath);
+        $this->deleteFileContainer($disk, dirname($filepath));
+    }
 
     /**
      * Physically remove the container of the uploaded file
@@ -32,10 +32,10 @@ class FileUploadMgmt extends FileUpload
      * @param $container  string
      * @return void
      */
-	private function deleteFileContainer($disk, $container)
-	{
-		if (empty(Storage::disk($disk)->allFiles($container))) {
+    private function deleteFileContainer($disk, $container)
+    {
+        if (empty(Storage::disk($disk)->allFiles($container))) {
             Storage::disk($disk)->deleteDirectory($container);
         }
-	}
+    }
 }
